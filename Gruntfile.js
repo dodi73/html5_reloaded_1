37859e32dec6773d9a89463a7db3148ef20bd848
toth8j <toth.jozsef8@mav-szk.hu>
2017-07-07 14:47:47 +0200

    HTML5_Reloaded 1-2. video: Környezet telepítése

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,49 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: [
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',              
+            'js/**/*.js'                          
+             ],
+        dest: 'build/js/<%= pkg.name %>.min.js'
       }
-    }
+    },
+
+    cssmin: {
+        options: {
+            mergeIntoShorthands: false,
+            roundingPrecision: -1
+        },
+        target: {
+            files: {
+                'build/css/output.css': [
+                    'node_modules/bootstrap/dist/css/bootstrap.min.css',
+                    'ode_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+                    'css/**/*.css'                    
+                ]
+            }
+        }
+    },
+      
+    watch: {
+        scripts: {
+            files: ['js/*.js', 'css/**/*.css'],
+            tasks: ['dev'],
+            options: {
+                spawn: false,
+            },
+        },
+    }  
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');    
+  grunt.loadNpmTasks('grunt-contrib-cssmin');        
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('dev', ['uglify', 'cssmin']);
+  grunt.registerTask('default', ['watch']);
 
 };
