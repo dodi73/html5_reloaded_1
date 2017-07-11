b6f7dc17aa928f0f08aa95de53805701b7366f8d
toth8j <toth.jozsef8@mav-szk.hu>
2017-07-11 11:11:12 +0200

    HTML5_Reloaded 1-2-3. video: Környezet telepítése+Bower+Angular

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 var reloaded = angular.module("reloaded", []);
 
-reloaded.controller("hello", ['$scope', function($scope) {
+reloaded.controller("hello", ['$scope', '$http', function($scope, $http) {
     $scope.name = "Jeffrey";
+    
+    $scope.users = [];
+    $http.get('json/user.json')
+        .then( function(serverData) {
+            //console.log( data );
+            $scope.users = serverData.data;
+    });
+    
 }]);
